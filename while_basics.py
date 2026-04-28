1678b9439357f422fddd206d9f6e1fdce2bdd5b3
juanapisa <jipisa@mail.austral.edu.ar>
2026-04-28 09:20:01 -0300

    Update while_basics.py

--- while_basics.py
+++ while_basics.py
@@ -9,7 +9,16 @@ def countdown(n):
     Ejemplo: countdown(0) -> [0]
     Ejemplo: countdown(-1) -> []
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    if n < 0:
+        return []
+
+    lista = []
+
+    while n >= 0:
+        lista.append(n)
+        n = n - 1
+    return lista
+
 
 
 def double_until(limit):
@@ -22,4 +31,13 @@ def double_until(limit):
     Ejemplo: double_until(1) -> [1]
     Ejemplo: double_until(0) -> []
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    if limit < 1:
+        return []
+
+    lista = []
+    n = 1
+
+    while n <= limit:
+        lista.append(n)
+        n = n * 2
+    return lista
